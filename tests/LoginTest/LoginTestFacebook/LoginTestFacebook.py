18264a89ab30499ad199f25c3e57724f240721a7
Médéric <mederic.andrieux@gmail.com>
2014-02-28 16:13:56 +0100

    add context/expectation/test

--- tests/LoginTest/LoginTestFacebook/LoginTestFacebook.py
+++ tests/LoginTest/LoginTestFacebook/LoginTestFacebook.py
@@ -3,13 +3,13 @@ import thic
 class LoginTestFacebook(thic.Test):
     def run(self):
         
-        self.context('Dans le ')
-        self.test('')
-        self.expectation('')        
+        self.set_context('Dans le ')
+        self.set_test('')
+        self.set_expectation('')
         self.compare_screen(0.8, 0, 56, 0, 0)
-        
-        
-        self.context('')
-        self.test('')
-        self.expectation('')    
+
+
+        self.set_context('')
+        self.set_test('')
+        self.set_expectation('')
         self.compare_screen(1)
